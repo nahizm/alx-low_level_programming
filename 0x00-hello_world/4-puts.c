34ee756c857d3f19bd8e2a28520fd161ddf49c90
nahizm <nahome.nb29@gmail.com>
2022-09-08 11:51:31 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print the string
- *
- * Description: main funciton
- */
+* main - print the string
+*
+* Description: main funciton
+*/
  int main(void)
+{
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
+}    
