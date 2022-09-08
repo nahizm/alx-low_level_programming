92e2627ef9bd59c73602bde5a8a6dccc7ef6e2fe
nahizm <nahome.nb29@gmail.com>
2022-09-08 11:58:47 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - printing in the put function
+*
+* Description: using the main function
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
