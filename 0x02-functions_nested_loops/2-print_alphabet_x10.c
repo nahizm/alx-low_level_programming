a38e3babc0a074a7c5495d112b6eda88015ac7a8
nahizm <nahome.nb29@gmail.com>
2022-09-13 11:52:02 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,6 @@
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
- * Dsadjasodjasodhjasodhaosdh
- *
  * Return: 0
  */
 void print_alphabet_x10(void)
@@ -12,7 +10,7 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (c= 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
