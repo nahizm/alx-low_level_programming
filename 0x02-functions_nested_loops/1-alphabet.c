70eb66e3928fa98aaef702d931c12f7e5df01286
nahizm <nahome.nb29@gmail.com>
2022-09-13 11:30:39 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'l c<= 'z'; c++)
+	{
+		_puchar(c);
+	}
+_putchar('\n');
+}
+
