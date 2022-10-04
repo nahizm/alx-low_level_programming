dbfb2a5eaa9e6d23bab7f25deb3696104dd0df7c
nahizm <nahome.nb29@gmail.com>
2022-10-04 01:00:21 -0700

    1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
-* create array - creates array
-* initialize it
-* @size: size of the array
-* @c: character
-* description: safdsdf
-* Return: NULL if size is zero
-* pointer to array.
+* create array - creates array of chars, and
+* initializes it with a specific char.
+*@size: size of the array
+*@c: character
+*Return: NULL if size is zero
+*pointer to array if everything is normal.
 */
 
 char *create_array(unsigned int size, char c)
@@ -23,6 +22,5 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	for (index = 0; index < size; index++)
 		array[index] = c;
-
 	return (array);
 }
