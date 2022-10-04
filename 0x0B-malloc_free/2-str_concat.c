4f565ae62693e9cbc15866d9228e5c435e993f62
nahizm <nahome.nb29@gmail.com>
2022-10-04 02:31:59 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
 /**
+ * @s1: s1
+ * @s2: s2
  * str_concat - a function that concatenates
  * Return: NULL if error
  */
@@ -24,7 +26,6 @@ char *str_concat(char *s1, char *s2)
 
 	for (index = 0; s1[index]; index++)
 		concat_str[concat_index++] = s1[index];
-	
 	for (index = 0; s2[index]; index++)
 		concat_str[concat_index++] = s2[index];
 	return (concat_str);
